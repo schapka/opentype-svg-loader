fdb3ee0d6fa3865df411f5136e50b04d769f97df
rschapka <rene@schapka.info>
2017-04-06 06:40:59 +0200

    Added option "sharedGlyphStore" to examples

--- examples/mustache/entry.js
+++ examples/mustache/entry.js
@@ -1,5 +1,5 @@
 'use strict';
 
-var headlineSVGString = require('./headline.ot.mustache?textAlign=left&size=12&lineHeight=1.1&decimalPlaces=2&attrs={"preserveAspectRatio": "xMaxYMax meet"}');
+var headlineSVGString = require('./headline.ot.mustache?textAlign=left&size=12&lineHeight=1.1&decimalPlaces=2&attrs={"preserveAspectRatio": "xMaxYMax meet"}&sharedGlyphStore=true');
 var wrapperEl = document.getElementById('mount');
 wrapperEl.innerHTML = headlineSVGString;
