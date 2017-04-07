8def3f55af80178b33f797361fccf7d8d719d3e1
rschapka <rene@schapka.info>
2017-04-07 06:44:12 +0200

    Created example svg output

---
+++ example-svg-output/build-svg-output.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var path = require('path');
+var fs = require('fs');
+
+var glob = require('glob');
+var merge = require('webpack-merge');
+
+var getDefaultOptions = require('../lib/getDefaultOptions');
+var renderSVG = require('../lib/renderSVG');
+
+var fontsDir = path.resolve(__dirname, 'fonts');
+var outputDir = path.resolve(__dirname, 'output');
+var sharedOptions = merge(getDefaultOptions(), {
+  size: 24,
+  lineHeight: 1.2,
+  textAlign: 'center',
+  text: [
+    'The European languages are members of the same family.',
+    'Their separate existence is a myth.',
+    'For science, music, sport, etc,',
+  ],
+  sharedGlyphStore: false,
+  attrs: {
+    test: true,
+    test2: false,
+    test3: 10,
+    test4: null,
+    test5: undefined,
+  },
+});
+
+glob(path.join(fontsDir, '*', '*.{ttf,otf}'), function(error, files) {
+  if (error) {
+    throw error;
+  } else {
+    files.forEach(function(fontFile) {
+      var fontName = path
+        .basename(fontFile)
+        .replace(/\.(ttf|otf)/i, '')
+        .replace(/[\s]+/i, '-')
+        .toLowerCase();
+
+      var outputFile = path.resolve(outputDir, fontName + '.svg');
+
+      var fonts = {};
+      fonts[fontName] = fontFile;
+
+      var options = merge(sharedOptions, {
+        fonts: fonts,
+        font: fontName,
+      });
+
+      renderSVG(options, function(error, svgString) {
+        if (!error && svgString) {
+          fs.writeFile(outputFile, svgString, 'utf8', function(error) {
+            if (error) {
+              console.log(error);
+              process.exit(1);
+            }
+          });
+        }
+      });
+    });
+  }
+});
