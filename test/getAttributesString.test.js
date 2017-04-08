84cbbf143e87766191b8113e4c8297ac33e971cf
rschapka <rene@schapka.info>
2017-04-08 04:04:23 +0200

    Added escaping for attributes

--- test/getAttributesString.test.js
+++ test/getAttributesString.test.js
@@ -12,8 +12,43 @@ describe('getAttributesString', function() {
     expect(typeof result).toBe('string');
   });
 
-  test('should return a valid attributes string including merged', function() {
+  test('should return a valid attributes string', function() {
+    var result = getAttributesString({ a: 'a' });
+    expect(result).toBe('a="a"');
+  });
+
+  test('should merge data', function() {
     var result = getAttributesString({ a: 'a' }, { b: 'b' });
     expect(result).toBe('a="a" b="b"');
+  });
+
+  test('should ignore undefined values', function() {
+    var result = getAttributesString({ c: undefined });
+    expect(result).toBe('');
+  });
+
+  test('should stringify null properly', function() {
+    var result = getAttributesString({ a: null });
+    expect(result).toBe('a="null"');
+  });
+
+  test('should stringify booleans properly', function() {
+    var result = getAttributesString({ a: true });
+    expect(result).toBe('a="true"');
+  });
+
+  test('should stringify numbers properly', function() {
+    var result = getAttributesString({ a: 100 });
+    expect(result).toBe('a="100"');
+  });
+
+  test('should stringify objects properly', function() {
+    var result = getAttributesString({ a: { bar: 'foo' } });
+    expect(result).toBe('a="{&quot;bar&quot;:&quot;foo&quot;}"');
+  });
+
+  test('should stringify arrays properly', function() {
+    var result = getAttributesString({ a: ['foo'] });
+    expect(result).toBe('a="[&quot;foo&quot;]"');
   });
 });
